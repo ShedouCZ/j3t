f58c3117fd8475c7ddb362b80b92a7363dbd1607
unknown <vojta.lanka@gmail.com>
2015-06-16 14:03:36 +0200

    sql

---
+++ Config/sql/tables.sql
@@ -0,0 +1,65 @@
+-- Adminer 4.2.1 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `gallery_albums`;
+CREATE TABLE `gallery_albums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `default_name` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `model_id` int(11) DEFAULT NULL,
+  `tags` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `gallery_pictures`;
+CREATE TABLE `gallery_pictures` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `size` bigint(20) NOT NULL,
+  `album_id` int(11) NOT NULL,
+  `main_id` int(11) DEFAULT NULL,
+  `caption` varchar(255) DEFAULT NULL,
+  `style` varchar(255) NOT NULL DEFAULT 'full',
+  `order` int(11) NOT NULL DEFAULT '9999999',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `folder_id` (`album_id`),
+  KEY `main_id` (`main_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `photos`;
+CREATE TABLE `photos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `ord` int(11) NOT NULL DEFAULT '999999',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8_czech_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
+  `mail` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+
+-- 2015-06-16 11:58:46
