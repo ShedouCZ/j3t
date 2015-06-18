fab994717a49037823d5742ea49af52b4947de38
Jan Ptacek <jan.ptacek@gmail.com>
2015-06-18 11:05:21 +0200

    date2sql

--- Model/AppModel.php
+++ Model/AppModel.php
@@ -21,4 +21,26 @@ App::uses('Model', 'Model');
  * @package       app.Model
  */
 class AppModel extends Model {
+	/**
+	 * various date to sql format
+	 */
+	public function date_to_sql($from) {
+		// filter via query string has '' on unset inputs
+		if ($from == '') return null;
+		// \xC2\xA0 is &nbsp; entity
+		$from = preg_replace("/(\s|\xc2\xa0)+/", '', $from);
+		// missing dot
+		if (preg_match('/^\d{1,2}\.\d{1,2}$/', $from))
+			$from = $from . '.';
+		// missing year
+		if (preg_match('/^\d{1,2}\.\d{1,2}.$/', $from))
+			$from = $from . date('Y');
+		($date = date_create_from_format('j#n#y', $from))       # d-m-y
+		|| ($date = date_create_from_format('j#n#Y', $from))    # d-m-yyyy
+		|| ($date = date_create_from_format('j#n#Y', date('j.n.Y', strtotime($from))));
+		return date_format($date, 'Y-m-d');
+	}
+	public function sql_to_date($date) {
+
+	}
 }
